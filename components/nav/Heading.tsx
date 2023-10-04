6ff7a142585f586a3b9057d607505db2cb297957
Mykael SIcard <112145455+mykaelsss@users.noreply.github.com>
2023-10-04 10:19:43 -0400

    Update Heading.tsx

--- components/nav/Heading.tsx
+++ components/nav/Heading.tsx
@@ -6,7 +6,7 @@ export const Heading = () => {
   return (
     <header className={styles.heading}>
       <MyLinks />
-      <OutlineButton onClick={() => window.open("/Mykael.Sicard.Resume.May.2023.pdf")}>
+      <OutlineButton onClick={() => window.open("/Mykael.Sicard.Resume.Sep.2023.1.pdf")}>
         My resume
       </OutlineButton>
     </header>
