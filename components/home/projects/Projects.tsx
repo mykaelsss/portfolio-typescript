05f76342d1b9adc4a6f301b67bbaca8860eebf62
Mykael SIcard <112145455+mykaelsss@users.noreply.github.com>
2024-02-06 00:10:44 -0500

    Update Projects.tsx

--- components/home/projects/Projects.tsx
+++ components/home/projects/Projects.tsx
@@ -18,39 +18,30 @@ export const Projects = () => {
 
 const projects = [
   {
-    title: "Endangered Animals",
-    imgSrc: "project-imgs/Endangered_Home_Page.png",
-    code: "https://github.com/mykaelsss/MERN-EndangeredAnimals-Deployment",
-    projectLink: "http://34.226.140.242/",
-    tech: ["React", "Node.JS",  "MaterialUI", "Express", "MongoDB", "Tailwind"],
-    description:
-      "Check out my awesome web app dedicated to endangered animals! It's my proudest project!",
+    title: "LinkList",
+    imgSrc: "project-imgs/linklist.png",
+    code: "https://github.com/mykaelsss/LinkList",
+    projectLink: "https://linkslisted.vercel.app/",
+    tech: ["Next.js", "Tailwind", "Supabase", "Vercel"],
+    description:"Developed a website akin to Linktree, providing users with a centralized platform to showcase and organize their various links in one location.",
     modalContent: (
       <>
         <p>
-          Endangered Animals is a web app I created to spread awareness to a select amount
-          of endangered animals in need of help.
+          This website was created with the goal of taking allowing user to seamlessly create, share and save links in a database for easy access later on.
         </p>
         <p>
-        The project was crafted with a dynamic tech stack, combining the power of JavaScript, React, Tailwind, and MaterialUI for the frontend.
-        On the backend, I utilized Node.JS and Express, creating a seamless experience.
-        For data storage, MongoDB was the chosen database, ensuring a robust and efficient solution.
+        The synergy of TypeScript, Next.js, Tailwind CSS, and Supabase forms a versatile foundation for crafting contemporary web applications.
+        TypeScript ensures a resilient and well-typed codebase, React empowers dynamic user interfaces,
+        Tailwind CSS simplifies styling through utility classes styling capabilities.
+        Widely recognized for enhancing developer productivity, code maintainability, and overall project scalability,
+        this tech stack is favored in modern web development.
         </p>
         <p>
-        To thoroughly test all aspects of my project, I employed Postman for comprehensive testing.
-        This included testing the routes from the backend to the database and ensuring smooth communication between the frontend and backend components.
-        </p>
-        <p>
-        As the full-stack developer for this project,
-        I had the responsibility of creating and managing both the frontend and backend.
-        Additionally, I oversaw the database management to ensure efficient data storage and retrieval.
-        By handling all these aspects, I ensured a cohesive and successful development process for the project.
-        </p>
-        <p>
-        Throughout the creation of this project, I devoted countless hours to extensive research on each animal species and carefully identified reputable organizations to support their welfare.
-        My primary goal was to ensure the website's accuracy, making it a reliable resource for users.
-        With a plethora of information available, I meticulously selected the most consistent answers for questions where multiple responses existed.
-        This dedication to accuracy and reliability was at the forefront of the website's development, guaranteeing its credibility as a valuable platform for animal enthusiasts and conservationists alike.
+          In my capacity as the frontend developer, I assumed a crucial role in establishing seamless connectivity among the project's frontend components.
+          Through meticulous coordination of the integration process,
+          I ensured the smooth transmission of data from the user interface to backend functionalities and, subsequently, to the database.
+          This comprehensive approach led to the creation of an efficient and cohesive
+          frontend application that not only met the project's requirements but also delivered users an uninterrupted and gratifying experience.
         </p>
       </>
     ),
@@ -59,7 +50,7 @@ const projects = [
     title: "LFG",
     imgSrc: "project-imgs/LFG_home_page.png",
     code: "https://github.com/mykaelsss/lfg_project",
-    projectLink: "http://3.90.37.67/",
+    projectLink: "http://18.233.168.135/",
     tech: ["Python", "Flask", "Bootstrap", "MySQL"],
     description:
       "A social network for gamers to find groups to play with.",
@@ -84,6 +75,39 @@ const projects = [
         I consider this project to be the most enjoyable one in my career, as it resonated with me on a personal level due to my passion for gaming.
         As a gamer myself, I felt a strong connection to the project, which fueled my enthusiasm and dedication throughout its development.
         I am extremely pleased with the final outcome, as it not only met all the objectives but also captured the essence of what makes gaming experiences so enjoyable.
+        </p>
+      </>
+    ),
+  },
+  {
+    title: "Cioso",
+    imgSrc: "project-imgs/Cioso.png",
+    code: "https://github.com/mykaelsss/CiosoNYC",
+    projectLink: "https://ciosonyc.vercel.app/",
+    tech: ["React", "Tailwind", "Sass", "Vercel"],
+    description:
+      "Collabarated with two FIT students to help bring there project to life.",
+    modalContent: (
+      <>
+        <p>
+        This website was created with the goal of taking my colleague's wireframe and bringing it to life as well as adding ideas and enhancing others.
+        </p>
+        <p>
+        Combining TypeScript, React, Tailwind CSS, and Sass provides a powerful and flexible foundation for developing modern web applications.
+        TypeScript ensures a robust and typed codebase, React facilitates the creation of dynamic user interfaces, Tailwind CSS streamlines styling with utility classes, and Sass enhances the styling capabilities with its features.
+        This tech stack is popular for its ability to improve developer productivity, code maintainability, and overall project scalability.
+        </p>
+        <p>
+        As the frontend developer, I played a pivotal role in ensuring seamless connectivity within the project's frontend components.
+        By meticulously orchestrating the integration process, I facilitated the smooth flow of data from the user interface to backend functionalities and, ultimately, to the database.
+        This holistic approach resulted in an efficient and cohesive frontend application. It not only met the project's requirements but also provided users with an uninterrupted and satisfying experience.
+        </p>
+        <p>
+          Throughout the development of this project, I invested a significant amount of passion into every facet.
+          My objective went beyond simply meeting functional requirements; I aimed to fashion a solution that resonated deeply with my vision and provided a source of joy for my colleagues.
+          Observing the final outcome of this website fills me with immense satisfaction.
+          The successful completion of this endeavor not only aligns with my initial aspirations but also brings me a profound sense of happiness, knowing that I've crafted something meaningful for my team members.
+          This experience has strengthened my belief in the ability of technology to transform personal sentiments into tangible and functional creations, making the journey exceptionally rewarding.
         </p>
       </>
     ),
@@ -123,63 +147,39 @@ const projects = [
     ),
   },
   {
-    title: "Cioso",
-    imgSrc: "project-imgs/Cioso.png",
-    code: "https://github.com/mykaelsss/CiosoNYC",
-    projectLink: "https://ciosonyc.vercel.app/",
-    tech: ["React", "Tailwind", "Sass", "Vercel"],
+    title: "Endangered Animals",
+    imgSrc: "project-imgs/Endangered_Home_page.png",
+    code: "https://github.com/mykaelsss/MERN-EndangeredAnimals-Deployment",
+    projectLink: "http://34.226.140.242/",
+    tech: ["React", "Node.JS",  "MaterialUI", "Express", "MongoDB", "Tailwind"],
     description:
-      "Collabarated with two FIT students to help bring there project to life.",
+      "Check out my awesome web app dedicated to endangered animals! It's my proudest project!",
     modalContent: (
       <>
         <p>
-        This website was created with the goal of taking my colleague's wireframe and bringing it to life as well as adding ideas and enhancing others.
+          Endangered Animals is a web app I created to spread awareness to a select amount
+          of endangered animals in need of help.
         </p>
         <p>
-        Combining TypeScript, React, Tailwind CSS, and Sass provides a powerful and flexible foundation for developing modern web applications.
-        TypeScript ensures a robust and typed codebase, React facilitates the creation of dynamic user interfaces, Tailwind CSS streamlines styling with utility classes, and Sass enhances the styling capabilities with its features.
-        This tech stack is popular for its ability to improve developer productivity, code maintainability, and overall project scalability.
+        The project was crafted with a dynamic tech stack, combining the power of JavaScript, React, Tailwind, and MaterialUI for the frontend.
+        On the backend, I utilized Node.JS and Express, creating a seamless experience.
+        For data storage, MongoDB was the chosen database, ensuring a robust and efficient solution.
         </p>
         <p>
-        As the frontend developer, I played a pivotal role in ensuring seamless connectivity within the project's frontend components.
-        By meticulously orchestrating the integration process, I facilitated the smooth flow of data from the user interface to backend functionalities and, ultimately, to the database.
-        This holistic approach resulted in an efficient and cohesive frontend application. It not only met the project's requirements but also provided users with an uninterrupted and satisfying experience.
+        To thoroughly test all aspects of my project, I employed Postman for comprehensive testing.
+        This included testing the routes from the backend to the database and ensuring smooth communication between the frontend and backend components.
         </p>
         <p>
-          Throughout the development of this project, I invested a significant amount of passion into every facet.
-          My objective went beyond simply meeting functional requirements; I aimed to fashion a solution that resonated deeply with my vision and provided a source of joy for my colleagues.
-          Observing the final outcome of this website fills me with immense satisfaction.
-          The successful completion of this endeavor not only aligns with my initial aspirations but also brings me a profound sense of happiness, knowing that I've crafted something meaningful for my team members.
-          This experience has strengthened my belief in the ability of technology to transform personal sentiments into tangible and functional creations, making the journey exceptionally rewarding.
-        </p>
-      </>
-    ),
-  },
-  {
-    title: "LinkList",
-    imgSrc: "project-imgs/linklist.png",
-    code: "https://github.com/mykaelsss/LinkList",
-    projectLink: "https://linkslisted.vercel.app/",
-    tech: ["Next.js", "Tailwind", "Supabase", "Vercel"],
-    description:"Developed a website akin to Linktree, providing users with a centralized platform to showcase and organize their various links in one location.",
-    modalContent: (
-      <>
-        <p>
-          This website was created with the goal of taking allowing user to seamlessly create, share and save links in a database for easy access later on.
+        As the full-stack developer for this project,
+        I had the responsibility of creating and managing both the frontend and backend.
+        Additionally, I oversaw the database management to ensure efficient data storage and retrieval.
+        By handling all these aspects, I ensured a cohesive and successful development process for the project.
         </p>
         <p>
-        The synergy of TypeScript, Next.js, Tailwind CSS, and Supabase forms a versatile foundation for crafting contemporary web applications.
-        TypeScript ensures a resilient and well-typed codebase, React empowers dynamic user interfaces,
-        Tailwind CSS simplifies styling through utility classes styling capabilities.
-        Widely recognized for enhancing developer productivity, code maintainability, and overall project scalability,
-        this tech stack is favored in modern web development.
-        </p>
-        <p>
-          In my capacity as the frontend developer, I assumed a crucial role in establishing seamless connectivity among the project's frontend components.
-          Through meticulous coordination of the integration process,
-          I ensured the smooth transmission of data from the user interface to backend functionalities and, subsequently, to the database.
-          This comprehensive approach led to the creation of an efficient and cohesive
-          frontend application that not only met the project's requirements but also delivered users an uninterrupted and gratifying experience.
+        Throughout the creation of this project, I devoted countless hours to extensive research on each animal species and carefully identified reputable organizations to support their welfare.
+        My primary goal was to ensure the website's accuracy, making it a reliable resource for users.
+        With a plethora of information available, I meticulously selected the most consistent answers for questions where multiple responses existed.
+        This dedication to accuracy and reliability was at the forefront of the website's development, guaranteeing its credibility as a valuable platform for animal enthusiasts and conservationists alike.
         </p>
       </>
     ),
